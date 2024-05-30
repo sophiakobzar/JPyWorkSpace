5d7fa47dd6da215fdfa87b9f96ad7e2cdd958856
sophiakobzar <31572998+sophiakobzar@users.noreply.github.com>
2024-05-30 15:52:53 -0500

    Update Factorial.java

--- FactorialCalculator/src/Factorial.java
+++ FactorialCalculator/src/Factorial.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number:");
+        System.out.println("Give a number and I will give factorial");
+        System.out.print("Enter a number: ");
         int number = scanner.nextInt();
         long factorial = calculateFactorial(number);
         System.out.println("The factorial of " + number + " is " + factorial);
