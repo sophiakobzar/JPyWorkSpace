6bf3acd09edda697d81361531d7a7c62f6a6bbae
sophiakobzar <31572998+sophiakobzar@users.noreply.github.com>
2024-06-13 14:27:15 -0500

    Create FlowControlExample.java

---
+++ JavaExample/src/com/cognixia/jump/FlowControlExample.java
@@ -0,0 +1,40 @@
+package com.cognixia.jump;
+
+public class FlowControlExample {
+	public static void main(String[] args)
+	{
+		ifStatment();
+	}
+	public static void ifStatment() {
+		int num1 = 2;
+		if(num1 > 0)
+		{
+			System.out.println(num1 + " is positive");
+		}
+		else if(num1<0)
+		{
+			System.out.println(num1 + " is negative");
+		}
+		else if(num1 > 2) // will never get because  2>0 first is
+		{
+			System.out.println(num1 + " is larger than 2");
+		}
+		else
+		{
+			System.out.println(num1 + " is zero");
+		}
+		
+		if(num1 % 2==0)
+		{
+			System.out.println(num1 +" is even");
+			if(num1 > 0)
+			{
+				System.out.println(num1 + " is positive");
+			}
+		}
+		else
+		{
+			System.out.println(num1 +" is odd");
+		}
+	}
+}
