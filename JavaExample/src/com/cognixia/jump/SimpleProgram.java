154cf9664680ee82f46a2f455c94076634ea75e2
sophiakobzar <31572998+sophiakobzar@users.noreply.github.com>
2024-06-19 20:43:33 -0500

    Update SimpleProgram.java

--- JavaExample/src/com/cognixia/jump/SimpleProgram.java
+++ JavaExample/src/com/cognixia/jump/SimpleProgram.java
@@ -1,20 +1,19 @@
 package com.cognixia.jump;
-
 import java.util.Scanner;
 
-public class SimpleProgram {
 
+
+public class SimpleProgram {
 	public static void main(String[] args) {
 		// scanner
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter your name  : ");
 		String name = sc.nextLine();
 		String reverseName = new StringBuilder(name).reverse().toString();
-		System.out.println("using built in   : " + reverseName);
-		System.out.println("created function : " + reverseString(name.toCharArray()));
+		System.out.println("Using built in   : " + reverseName);
+		System.out.println("Created function : " + reverseString(name.toCharArray()));
 		sc.close();
 	}
-	
 	public static String reverseString(char[] str)
 	{
 		char[] reCharacters = new char[str.length];
