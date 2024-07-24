7b445576bc6f5f2d44fcbdb730abac2acf8bb9d9
sophiakobzar <31572998+sophiakobzar@users.noreply.github.com>
2024-07-24 13:34:17 -0500

    adding more yellow letter logic "right letter wrong place"

--- JavaExample/src/com/cognixia/jump/WordleGame.java
+++ JavaExample/src/com/cognixia/jump/WordleGame.java
@@ -1,5 +1,4 @@
 package com.cognixia.jump;
-
 import java.util.Scanner;
 
 public class WordleGame {
@@ -11,7 +10,10 @@ public class WordleGame {
 
     public void playGame() {
         Scanner scanner = new Scanner(System.in);
+
         for (int i = 0; i < 6; i++) {
+            boolean[] found = new boolean[26];  // Reset the found array for each guess
+
             System.out.println("Enter your guess:");
             String guess = scanner.nextLine();
             if (guess.equals(secretWord)) {
@@ -20,21 +22,44 @@ public class WordleGame {
             } else {
                 StringBuilder hint = new StringBuilder();
 
-                for (int j = 0; j < secretWord.length(); j++) {
-                    if (guess.charAt(j) == secretWord.charAt(j)) {
+                for (int j = 0; j < guess.length(); j++) {
+                    if (j < secretWord.length() && secretWord.charAt(j) == guess.charAt(j)) {
                         hint.append(guess.charAt(j) + "(Green) ");
-                    } else if (secretWord.contains(String.valueOf(guess.charAt(j)))) {
+                        found[guess.charAt(j) - 'a'] = true;  // Mark the character as found
+                    } else if (j < secretWord.length() && !found[guess.charAt(j) - 'a'] && secretWord.contains(String.valueOf(guess.charAt(j)))) {
                         hint.append(guess.charAt(j) + "(Yellow) ");
+                        found[guess.charAt(j) - 'a'] = true;  // Mark the character as found
                     } else {
                         hint.append(guess.charAt(j) + "(Gray) ");
                     }
                 }
 
-                System.out.println(hint.toString());
+                System.out.println(hint);
             }
-
         }
         System.out.println("Sorry, you didn't guess the word. The correct word was: " + secretWord);
+    }
+
+
+
+    public static boolean foundAllLetter(String secretWord, String guess, char letter) {
+        int countInSecret = 0;
+        int countInGuess = 0;
+
+        // Count occurrences of the letter in the secret word
+        for (int i = 0; i < secretWord.length(); i++) {
+            if (secretWord.charAt(i) == letter) {
+                countInSecret++;
+            }
+        }
+        // Count occurrences of the letter in the guess
+        for (int i = 0; i < guess.length(); i++) {
+            if (guess.charAt(i) == letter) {
+                countInGuess++;
+            }
+        }
+        // Return true if the counts match, false otherwise
+        return countInSecret == countInGuess;
     }
 
     public static void main(String[] args) {
